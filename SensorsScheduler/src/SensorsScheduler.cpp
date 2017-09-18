78dfdd78fe439930ca7fb9d96b7d99a22b04216e
AngeloTrotta85 <angelo@magilia.com>
2017-09-18 17:39:02 +0200

    first

---
+++ SensorsScheduler/src/SensorsScheduler.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SensorsScheduler.cpp
+// Author      : Angelo Trotta
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
